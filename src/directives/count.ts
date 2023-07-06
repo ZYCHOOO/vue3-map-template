99301b509a0e55346f47c76149acdbcadea47b7e
曾逸超 <zengyichao@digibms.com>
2023-07-06 11:51:47 +0800

    fix：count-directive 数字显示错误

--- src/directives/count.ts
+++ src/directives/count.ts
@@ -4,22 +4,21 @@ import { thousandsFormat } from '@/utils/index'
 export default function installCountDirective(app: App) {
   const countDirective:Directive = {
     updated(el, binding) {
-      const { arg, value } = binding
-      const resultNum = Number(arg)
-      // 单位名称
-      const unitName = value.unit || ''
+      const { oldValue, value } = binding
+      if (oldValue.count === value.count) return
+      const { count: resultNum, unit: unitName } = value
       // 动画间隔
       const duration = 30
       // 步长 数据增加间隔
       const step = Number((resultNum / duration).toFixed(0))
-      let count = 0
+      let resultCount = 0
       let timer:any
       timer = setInterval(() => {
-        count = count + step
-        el.innerHTML = `${thousandsFormat(count)}<span class="v-count-unit">${unitName}</span>`
-        if (count > resultNum) {
-          count = resultNum
-          el.innerHTML = `${thousandsFormat(count)}<span class="v-count-unit">${unitName}</span>`
+        resultCount = resultCount + step
+        el.innerHTML = `${thousandsFormat(resultCount)}<span class="v-count-unit">${unitName}</span>`
+        if (Number(resultCount) > resultNum) {
+          resultCount = resultNum
+          el.innerHTML = `${thousandsFormat(resultCount)}<span class="v-count-unit">${unitName}</span>`
           // 清空定时器
           clearInterval(timer)
           timer = null
